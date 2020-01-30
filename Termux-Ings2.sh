db03836a4eb3fef9b5d8fafb5e2091def00d0a7b
caplintamfan <60221920+caplintamfan@users.noreply.github.com>
2020-01-31 00:52:05 +0700

    Update2

---
+++ Termux-Ings2.sh
@@ -0,0 +1,342 @@
+#!system/xbin/bash
+#jangan di record
+clear
+echo "\033[37;1m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[31;1m             .:okOOOkdc'           'cdkOOOko:."
+echo "\033[31;1m          . xOOOOOOOOOOOOc       cOOOOOOOOOOOOx."
+echo "\033[31;1m          :OOOOOOOOOOOOOOOk,   ,kOOOOOOOOOOOOOOO:"
+echo "\033[31;1m          OOOOOOOOOkkkkOOOOO: :OOOOOOOOOOOOOOOOOO'"
+echo "\033[31;1m         oOOOOOOOO.    .oOOOOoOOOOl.    ,OOOOOOOOo"
+echo "\033[31;1m         dOOOOOOOO.      .cOOOOOc.      ,OOOOOOOOx"
+echo "\033[31;1m         lOOOOOOOO.         ;d;         ,OOOOOOOOl"
+echo "\033[31;1m         .OOOOOOOO.   .;           ;    ,OOOOOOOO."
+echo "\033[31;1m          cOOOOOOO.   .OOc.     'oOO.   ,OOOOOOOc"
+echo "\033[37;1m           oOOOOOO.   .OOOO.   :OOOO.   ,OOOOOOo"
+echo "\033[37;1m            lOOOOO.   .OOOO.   :OOOO.   ,OOOOOl"
+echo "\033[37;1m             ;OOOO'   .OOOO.   :OOOO.   ;OOOO;"
+echo "\033[37;1m              .dOOo   .OOOOocccxOOOO.   xOOd."
+echo "\033[37;1m                ,kOl  .OOOOOOOOOOOOO. .dOk,"
+echo "\033[37;1m                  :kk;.OOO MASTER OOO.cOk:"
+echo "\033[37;1m                    ;kOOOOOOOOOOOOOOOk:"
+echo "\033[37;1m                     ,xOOOOOOOOOOOx,"
+echo "\033[37;1m                       .lOOOOOOOl."
+echo "\033[37;1m                          ,dOd,"
+echo "\033[37;1m      WELCOME               ."
+echo "\033[37;1m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[36;1m        ╔═══╗╔╗  ╔╗╔═══╗╔════╗╔═══╗╔═╗╔═╗   versi:2.0"
+echo "\033[36;1m        ║╔═╗║║╚╗╔╝║║╔═╗║║╔╗╔╗║║╔══╝║║╚╝║║   Author : Mr.IngsS"
+echo "\033[36;1m        ║╚══╗╚╗╚╝╔╝║╚══╗╚╝║║╚╝║╚══╗║╔╗╔╗║   Myteam : BlackCyberTeam"
+echo "\033[32;1m        ╚══╗║ ╚╗╔╝ ╚══╗║  ║║  ║╔══╝║║║║║║   Contact: 089628032577"
+echo "\033[32;1m        ║╚═╝║  ║║  ║╚═╝║  ║║  ║╚══╗║║║║║║"
+echo "\033[32;1m        ╚═══╝  ╚╝  ╚═══╝  ╚╝  ╚═══╝╚╝╚╝╚╝"
+echo "\033[37;1m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[1;37m   •    BERPIKIRLAH SEBAGAI PEMULA,MAKA DUNIA AKAN TERBUKA UNTUK MU"
+echo "\033[1;37m   •    GUNAKAN TOOLS INI DENGAN BIJAK"
+echo "\033[1;37m   •    KARNA KAMI TIDAK AKAN TANGGUNG JAWAB ATAS PERBUATAN KALIAN"
+echo "\033[1;37m   •    SUBSCRIBE CHANEL YOUTUBE [> SAYDOG <]"
+echo "\033[37;1m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[1;31m        ████████╗ ██████╗  ██████╗ ██╗     ███████╗"
+echo "\033[1;31m        ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝"
+echo "\033[1;31m           ██║   ██║   ██║██║   ██║██║     ███████╗"
+echo "\033[1;37m           ██║   ██║   ██║██║   ██║██║     ╚════██║"
+echo "\033[1;37m           ██║   ╚██████╔╝╚██████╔╝███████╗███████║"
+echo "\033[1;37m           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
+echo "\033[37;1m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[0;32m╔═══════════════════════════════════════════════════╗"
+echo "\033[0;32m║                                                   ║"
+echo "\033[0;32m║          ¤ SILAHKAN PILIH TOOLS NYA ¤             ║"
+echo "\033[0;32m║                                                   ║"
+echo "\033[0;32m║ •       {1}. HACKED FACEBOOK           ✔          ║"
+echo "\033[0;32m║   •     {2}. BTUTEFORCE FACEBOOK        ✔         ║"
+echo "\033[0;32m║ •       {3}. CREATE VIRUS ANDROID      ✔          ║"
+echo "\033[0;32m║   •     {4}. METASPLOIT-HELPER          ✔         ║"
+echo "\033[0;32m║ •       {5}. NEWSX INSTALLER           ✔          ║"
+echo "\033[0;32m║   •     {6}. EVIL CREATE FRAMEWORK      ✔         ║"
+echo "\033[0;32m║ •       {7}. SAYDOG                    ✔          ║"
+echo "\033[0;32m║   •     {8}. DARK-SMS WORK              ✔         ║"
+echo "\033[0;32m║ •       {9}. TERKEY                    ✔          ║"
+echo "\033[0;32m║   •     {10}.INFO TEAM                  ✔         ║"
+echo "\033[0;32m║ •       {11}.INSTAGRAM-MASSAL          ✔          ║"
+echo "\033[0;32m║   •     {12}.INSTALLING TERMUX-THEME    ✔         ║"
+echo "\033[0;32m║ •       {13}.INSTALLING SPAMMER EMAIL  ✔          ║"
+echo "\033[0;32m║   •     {14}.INSTALLING SPAMER          ✔         ║"
+echo "\033[0;32m║ •       {15}.FB TARGET                 ✔          ║"
+echo "\033[0;32m║   •     {16}.X-random                   ✔         ║"
+echo "\033[0;32m║ •       {17}.B-VIRUS                   ✔          ║"
+echo "\033[0;32m║   •     {18}.RED_HAWK                   ✔         ║"
+echo "\033[0;32m║ •       {19}.INSTALLING BRUTEFORCE FB2 ✔          ║"
+echo "\033[0;32m║   •     {20}.INSTALLING BRUTEFORCE IG1  ✔         ║"
+echo "\033[0;32m║ •       {21}.EXIT/CLOSE                ✔          ║"
+echo "\033[0;32m║                                                   ║"
+echo "\033[0;32m║                                                   ║"
+echo "\033[0;32m║    TERIMA KASIH SUDAH MENGGUNKAN TOOLS KAMI :)    ║"
+echo "\033[0;32m║═══════════════════════════════════════════════════╝"
+        read -p"║_____SILAHKAN PILIH TOOLS : " pilih
+if [ $pilih = "1" ]
+then
+echo "Installing HACKED FACEBOOK....."
+cd $HOME
+pkg install python2
+pkg install git
+git clone https://github.com/bukanhacker/Sad
+cd Sad
+python2 Sad.py
+fi
+
+if [ $pilih = "2" ]
+then
+echo "Installing BruteForce Fb....."
+cd $HOME
+apt install python2
+pkg install pip2
+pip2 install bs4
+pip2 install requests
+git clone https://github.com/FR13ND8/BruteFb.git
+cd BruteFb
+python2 brute.py
+fi
+
+if [ $pilih = "3" ]
+then
+echo "Installing Create Virus Android....."
+cd $HOME
+pkg install git wget curl
+pkg install python2
+git clone https://github.com/saydog/Virus-droid-creator.git
+cd Virus-droid-creator
+python2 dog.py
+fi
+
+if [ $pilih = "4" ]
+then
+echo "Installing Metasploit-Helper....."
+cd $HOME
+pkg install git
+pkg install python2
+git clone https://github.com/saydog/Metasploit-Helper.git
+cd Metasploit-Helper
+sh install.sh
+fi
+
+if [ $pilih = "5" ]
+then
+echo "Installing NewsX Installer....."
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/pengangguranmuda/NewsX.git
+cd NewsX
+sh newsx.sh
+fi
+
+if [ $pilih = "6" ]
+then
+echo "Installing Evil Create Virus....."
+cd $HOME
+pkg install python2
+pkg install git
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+cd Evil-create-framework
+sh install.sh
+fi
+
+if [ $pilih = "7" ]
+then
+echo "Installing Saydog....."
+cd $HOME
+pkg update && pkg upgrade
+pkg install pip2
+pkg install git
+pip2 install colorama
+git clone https://github.com/saydog/SAYDOG.git
+cd SAYDOG
+sh install.sh
+fi
+
+if [ $pilih = "8" ]
+then
+echo "Installing Dark-sms work....."
+cd $HOME
+pkg install git
+pkg install python
+pkg install pip
+pip install requests
+git clone https://github.com/N74NK/sms.git
+cd sms
+python sms.py
+fi
+
+if [ $pilih = "9" ]
+then
+echo "Installing Terkey....."
+cd $HOME
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/karjok/terkey.git
+cd terkey
+python2 terkey.py
+fi
+
+if [ $pilih = "10" ]
+then
+echo "VISIT INFO TEAM....."
+echo "\033[0;32m|===========We Are BlackCyberTeam============|"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|    JIKA MENGHINA KAMI KALIAN SUDAH HOBI    |"
+echo "\033[0;32m|       MAKA DALAM BIDANG BODOAMAT LAH       |"
+echo "\033[0;32m|       KAMI SUDAH MENDADI AHLI              |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|         WE ARE BLACKCYBERTEAM...           |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|       • Mr.IngsS                           |"
+echo "\033[0;32m|       • Mr.ClayBot                         |"
+echo "\033[0;32m|       • Mr.H4CKYOU7H                       |"
+echo "\033[0;32m|       • Mr.J4NC0K                          |"
+echo "\033[0;32m|       ° Mr.Style                           |"
+echo "\033[0;32m|       • Mr.B17CH                           |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|============================================|"
+fi
+
+if [ $pilih = "11" ]
+then
+echo "Installing Instagram-Massal....."
+cd $HOME
+pkg update && apt upgrade
+pkg install bash
+pkg install curl
+pkg install git
+git clone https://github.com/novalattasya/instahack.git
+cd instahack
+bash ig.sh
+fi
+
+if [ $pilih = "12" ]
+then
+echo "Installing Termux-Theme....."
+cd $HOME
+pkg install python2
+pkg install git
+pkg install lolcat
+pkg install figlet
+git clone https://github.com/saydog/termux-theme.git
+cd termux-theme
+python2 theme.py
+fi
+
+if [ $pilih = "13" ]
+then
+echo "Installing Spammer email....."
+cd $HOME
+pkg install git
+pkg install python3
+pip install mechanize
+pip install requests
+git clone https://github.com/Zen-Clay/Mailspamz.git
+cd Mailspamz
+python3 Mailspamz.py
+fi
+
+if [ $pilih = "14" ]
+then
+echo "Installing Spammer......"
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/pengangguranmuda/SPAMMER.git
+cd SPAMMER
+sh install.sh
+fi
+
+if [ $pilih = "15" ]
+then
+echo "Installing FB TARGET......"
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pkg install python
+git clone https://github.com/storiku/fbtarget.git
+cd fbtarget
+python2 target.py
+fi
+
+if [ $pilih = "16" ]
+then
+echo "Installing X-Random......"
+cd $HOME
+pkg install python2
+pkg install git
+git clone https://github.com/kucing-hitam/X-random.git
+cd X-random
+sh XXX.sh
+fi
+
+if [ $pilih = "17" ]
+then
+echo "Installing B-VIRUS......"
+cd $HOME
+pkg install python2
+pkg install git
+pkg update && pkg upgrade
+git clone https://github.com/BlackCyberAnonim/B-VIRUS.git
+cd B-VIRUS
+python2 B-VIRUS.py
+fi
+
+if [ $pilih = "18" ]
+then
+echo "Installing RED_HAWK......"
+cd $HOME
+pkg install python2
+pkg install python
+pkg install php
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pilih = "19" ]
+then
+cd $HOME
+echo "Installing BruteForce Facebook2......"
+pkg install mechanize
+pkg install requests
+pkg install git
+pkg install python2
+pip2 install requests
+pip install requests
+pip2 install mechanize
+git clone https://github.com/Din-zUgex95/FBbruT.git
+cd FBbruT
+python2 brute-FB.py
+fi
+
+if [ $pilih = "20" ]
+then
+cd $HOME
+echo "Installing Bruteforce IG1....."
+pkg install git
+pkg install python
+pkg install python2
+pkg install nano
+pip install requests
+pip2 install mechanize
+git clone https://github.com/saydog/HEK-IG.git
+cd HEK-IG
+python2 dog.py
+fi
+
+if [ $pilih = "21" ]
+then
+cd $HOME
+echo "\033[31;1mTERIMA KASIH SUDAH MENGGUNAKAN TOOLS KAMI :)"
+echo "\033[31;1mTOLONG HARGAI PEMBUAT SCRIPT INI [ MR.INGSS ]."
+echo "\033[31;1mJIKA ADA ERROR ATAU TIDAK DI PAHAMI SILAHKAN HUBUNGI ADMIN"
+echo "\033[31;1mDAN JANGAN DI JUAL BELIKAN SCRIPT INI/RECORD PROGRAM PADA SCRIPT INI TANPA IZIN ADMI>
+echo "\033[31;1mWE ARE BLACKCYBERTEAM...."
+sleep 5
+exit
+fi
